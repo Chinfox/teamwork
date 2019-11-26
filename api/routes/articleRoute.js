2ef7939e8471485dd61931840ab601a1e4e2cd17
chinedum onyema <onyemachinedum@gmail.com>
2019-11-26 09:53:31 +0100

    feature(view-article): enable article view/fetch
    this will enable employees view a specific article by ID
    
    [closes #17]

--- api/routes/articleRoute.js
+++ api/routes/articleRoute.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const articleController = require('../controllers/articleController');
 
 router.post('/', articleController.create);
-// router.get('/:id', articleController.getOne);
+router.get('/:id', articleController.getOne);
 router.patch('/:id', articleController.edit);
 router.delete('/:id', articleController.remove);
 router.post('/:id/comment', articleController.makeComment);
