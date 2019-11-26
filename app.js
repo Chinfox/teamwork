e75622abc59069ce146da26e30fe175b75e95e77
chinedum onyema <onyemachinedum@gmail.com>
2019-11-26 18:04:33 +0100

    feature(file-upload): enable file middleware
    [starts #9]

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const cors = require('cors');
-
+const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -12,6 +10,7 @@ const app = express();
 const port = process.env.PORT || '4000';
 const userRoutes = require('./api/routes/userRoute');
 const articleRoutes = require('./api/routes/articleRoute');
+const gifRoutes = require('./api/routes/gifRoute');
 
 app.use(cors());
 
@@ -22,9 +21,13 @@ app.use(
   }),
 );
 
+app.use(fileUpload({
+  useTempFiles: true,
+}));
 
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/articles', articleRoutes);
+app.use('/api/v1/gifs', gifRoutes);
 
 app.use('/', (req, res) => {
   res.json({ message: 'Welcome to the Teamwork API !' });
