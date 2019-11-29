cd4437a2044916650d6b31a1ed590f6765411028
chinedum onyema <onyemachinedum@gmail.com>
2019-11-29 10:55:00 +0100

    bug(travis-ci) debug build fail

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,5 +1,6 @@
 const { verifyToken } = require('../lib/tokenManager');
 
+// eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
